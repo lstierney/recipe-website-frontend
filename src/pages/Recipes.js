c2b47647c1a78cf5b9793c676ae1190b1b70ef8d
Lawrence Tierney <lawrence.tierney@protonmail.com>
2023-03-09 21:26:26 +0000

    MVR-38 Component to display list of Recipes

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,20 +1,60 @@
 import {Link} from "react-router-dom";
-
-const RECIPES = [
-    {id: '1', title: 'Recipe 1'},
-    {id: '2', title: 'Recipe 2'},
-    {id: '6', title: 'Recipe 6'},
-];
+import {useEffect, useState} from "react";
+import {uiActions} from "../store/ui-slice";
+import {useDispatch} from "react-redux";
 
 const Recipes = () => {
+    const [recipesList, setRecipesList] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch("http://localhost:8080/recipes/list")
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("Failed to fetch data");
+                }
+            })
+            .then((data) => {
+                setRecipesList(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setError(error);
+                setIsLoading(false);
+                dispatch(uiActions.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: error.message
+                }));
+            });
+    }, [dispatch]);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    if (!recipesList) {
+        return null;
+    }
+
+
     return (
         <>
             <h1>The recipes page!</h1>
             <ul>
-                {RECIPES.map(recipe =>
+                {recipesList.map(recipe =>
                     <li key={recipe.id}>
                         <Link to={`/recipes/${recipe.id}`}>
-                            {recipe.title}
+                            {recipe.name}
                         </Link>
                     </li>
                 )}
