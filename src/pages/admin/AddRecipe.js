5741254615a0c1dec289cb192b99d1f43079550d
Lawrence Tierney <lawrence.tierney@protonmail.com>
2023-03-13 22:39:47 +0000

    MVR-41 Add tags to recipe

--- src/pages/admin/AddRecipe.js
+++ src/pages/admin/AddRecipe.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import IngredientInput from "../../components/admin/IngredientInput";
 import IngredientsList from "../../components/recipe/IngredientsList";
 import MethodStepsList from "../../components/recipe/MethodStepsList";
 import MethodStepInput from "../../components/admin/MethodStepInput";
 import {useDispatch, useSelector} from "react-redux";
-import {uiActions} from "../../store/ui-slice";
+import {addRecipe} from "../../store/recipes-actions";
 
 
 const AddRecipe = () => {
@@ -15,91 +15,125 @@ const AddRecipe = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [cookingTime, setCookingTime] = useState(0);
+    const [selectedTags, setSelectedTags] = useState([])
+    const [availableTags, setAvailableTags] = useState([])
 
     const units = useSelector(state => state.meta.units);
+    const metaTags = useSelector(state => state.meta.tags);
     const dispatch = useDispatch();
 
-    const addRecipeHandler = async () => {
+    useEffect(() => {
+        setAvailableTags(metaTags);
+    }, [metaTags]);
+
+    const addRecipeHandler = () => {
         const recipe = {
             // Set to undef so that the backend will fail "NOT NULL" checks
             name: name.length !== 0 ? name : undefined,
             description: description.length !== 0 ? description : undefined,
             cookingTime: +cookingTime > 0 ? +cookingTime : undefined,
             ingredients: ingredients,
-            methodSteps: methodSteps
+            methodSteps: methodSteps,
+            tags: selectedTags
         };
         if (window.confirm(JSON.stringify(recipe, null, 2))) {
-            let response;
-            try {
-                response = await fetch("http://localhost:8080/recipes", {
-                    method: 'POST',
-                    body: JSON.stringify(recipe),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-                    if (!response.ok) {
-                        throw new Error('Sending Recipe data failed');
-                    }
-                } catch (Error) {
-                    const errorMessage = response ? await response.text() : '';
-                    dispatch(uiActions.showNotification({
-                        status: 'error',
-                        title: 'Error!',
-                        message: "Could not POST Recipe data: " + errorMessage
-                    }));
-                }
-                dispatch(uiActions.showNotification({
-                    status: 'success',
-                    title: 'Success!',
-                    message: "Successfully added Recipe!"
-                }));
-            }
+            dispatch(addRecipe(recipe));
         }
-        const onAddIngredientHandler = (ingredient) => {
-            const newIngredients = ingredients.slice();
-            newIngredients.push(ingredient);
-            setIngredients(newIngredients);
-        }
-        const onRemoveIngredientHandler = (description) => {
-            const filteredIngredients = ingredients.filter(ingredient => ingredient.description !== description);
-            setIngredients(filteredIngredients)
-        }
+    }
 
-        const onAddMethodStepHandler = (description) => {
-            const newMethodSteps = methodSteps.slice();
-            newMethodSteps.push({
-                ordering: methodStepOrderingId,
-                description: description
-            });
-            setMethodSteps(newMethodSteps);
-            setMethodStepOrderingId(methodStepOrderingId + 1);
-        }
+    const onAddIngredientHandler = (ingredient) => {
+        const newIngredients = ingredients.slice();
+        newIngredients.push(ingredient);
+        setIngredients(newIngredients);
+    }
+    const onRemoveIngredientHandler = (description) => {
+        const filteredIngredients = ingredients.filter(ingredient => ingredient.description !== description);
+        setIngredients(filteredIngredients)
+    }
 
-        return (
-            <div>
-                <h1>Add Recipe</h1>
-                <section>
-                    <h2>General</h2>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" value={name} onChange={e => setName(e.target.value)}/><br/>
-                    <label htmlFor="description">Description:</label><br/>
-                    <textarea name="description" rows="10" cols="80" value={description}
-                              onChange={e => setDescription(e.target.value)}/><br/>
-                    <label htmlFor="cookingTime">Cooking Time:</label>
-                    <input type="number" name="cookingTime" value={cookingTime}
-                           onChange={e => setCookingTime(+e.target.value)}/>
-                </section>
-                <section>
-                    <h2>Ingredients</h2>
-                    <IngredientsList ingredients={ingredients} onRemoveIngredientHandler={onRemoveIngredientHandler}
-                                     units={units}/>
-                    <IngredientInput onAdd={onAddIngredientHandler} units={units}/>
-                </section>
-                <section>
-                    <h2>Method Steps</h2>
-                    <MethodStepsList methodSteps={methodSteps}/>
-                    <MethodStepInput onAdd={onAddMethodStepHandler}/>
+    const onAddMethodStepHandler = (description) => {
+        const newMethodSteps = methodSteps.slice();
+        newMethodSteps.push({
+            ordering: methodStepOrderingId,
+            description: description
+        });
+        setMethodSteps(newMethodSteps);
+        setMethodStepOrderingId(methodStepOrderingId + 1);
+    }
+
+    const onAddTagHandler = (id) => {
+        // Remove tag from available list
+        const selectedTag = availableTags.filter(tag => tag.id === id)[0];
+        setAvailableTags(availableTags.filter(tag => tag.id !== id));
+        // add tag to recipe list
+        const recipeList = selectedTags.slice();
+        recipeList.push(selectedTag);
+        setSelectedTags(recipeList);
+    }
+
+    const onRemoveTagHandler = (id) => {
+        // Remove tag from available list
+        const selectedTag = selectedTags.filter(tag => tag.id === id)[0];
+        setSelectedTags(selectedTags.filter(tag => tag.id !== id));
+        // add tag to recipe list
+        const availableList = availableTags.slice();
+        availableList.push(selectedTag);
+        setAvailableTags(availableList);
+    }
+
+    const availableTagsContent = () => {
+        let content;
+        if (availableTags.length === 0) {
+            content = 'No tags available';
+        } else {
+            content = availableTags.map(tag => <button key={tag.id} type="button"
+                                                       onClick={() => onAddTagHandler(tag.id)}>{tag.name}</button>);
+        }
+        return content;
+    }
+
+    const selectedTagsContent = () => {
+        let content;
+        if (selectedTags.length === 0) {
+            content = 'No tags selected';
+        } else {
+            content = selectedTags.map(tag => <button key={tag.id} type="button"
+                                                      onClick={() => onRemoveTagHandler(tag.id)}>{tag.name}</button>);
+        }
+        return content;
+    }
+
+    return (
+        <div>
+            <h1>Add Recipe</h1>
+            <section>
+                <h2>General</h2>
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" value={name} onChange={e => setName(e.target.value)}/><br/>
+                <label htmlFor="description">Description:</label><br/>
+                <textarea name="description" rows="10" cols="80" value={description}
+                          onChange={e => setDescription(e.target.value)}/><br/>
+                <label htmlFor="cookingTime">Cooking Time:</label>
+                <input type="number" name="cookingTime" value={cookingTime}
+                       onChange={e => setCookingTime(+e.target.value)}/>
+            </section>
+            <section>
+                <h2>Tags</h2>
+                <h3>Available</h3>
+                {availableTagsContent()}
+                <h3>Selected</h3>
+                {selectedTagsContent()}
+            </section>
+            <section>
+                <h2>Ingredients</h2>
+                <IngredientsList ingredients={ingredients} onRemoveIngredientHandler={onRemoveIngredientHandler}
+                                 units={units}/>
+                <IngredientInput onAdd={onAddIngredientHandler} units={units}/>
+            </section>
+            <section>
+                <h2>Method Steps</h2>
+                <MethodStepsList methodSteps={methodSteps}/>
+                <MethodStepInput onAdd={onAddMethodStepHandler}/>
                 </section>
                 <button type="submit" onClick={addRecipeHandler}>Add Recipe</button>
 
