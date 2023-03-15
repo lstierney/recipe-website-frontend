612dda4fff1720ce93e525955b7c50fc14c1cc29
Lawrence Tierney <lawrence.tierney@protonmail.com>
2023-03-15 00:09:39 +0000

    MVR-82 Tags page with search by tag.

--- src/routes/main-route.js
+++ src/routes/main-route.js
@@ -6,6 +6,7 @@ import Recipe from "../pages/Recipe";
 import AddRecipe from "../pages/admin/AddRecipe";
 import AdminHomePage from "../pages/admin/AdminHomePage";
 import ManageTags from "../pages/admin/ManageTags";
+import Tags from "../pages/Tags";
 
 export const routes = [
     {
@@ -16,7 +17,8 @@ export const routes = [
             {index: true, element: <HomePage/>},
             {path: 'recipes', element: <Recipes/>},
             {path: 'recipes/:recipeId', element: <Recipe/>},
-            {path: 'recipes/add', element: <AddRecipe/>}
+            {path: 'recipes/add', element: <AddRecipe/>},
+            {path: 'tags', element: <Tags/>},
         ]
     },
     {
