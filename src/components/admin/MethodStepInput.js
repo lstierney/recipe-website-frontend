44a5dd0591af8bfa1d234f32022ace3084b85afe
Lawrence Tierney <lawrence.tierney@protonmail.com>
2023-07-18 15:01:19 +0100

    MVR-207 On adding an Ingredient, Step or Note the associated textarea will clear and focus (ready for next input)

--- src/components/admin/MethodStepInput.js
+++ src/components/admin/MethodStepInput.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import Button from "../button/Button";
 
 const MethodStepInput = (props) => {
     const [description, setDescription] = useState('');
+    const textAreaRef = useRef(null);
+
+    const onAdd = () => {
+        props.onAdd(description);
+        textAreaRef.current.value = '';
+        textAreaRef.current.focus();
+    }
 
     return (
         <div>
-            <textarea cols="60" onChange={e => setDescription(e.target.value)} name="methodStep"/>
-            <Button type="button" onClick={() => props.onAdd(description)}>Add</Button>
+            <textarea ref={textAreaRef} cols="60" onChange={e => setDescription(e.target.value)} name="methodStep"/>
+            <Button type="button" onClick={onAdd}>Add</Button>
         </div>
     );
 };
--- src/components/admin/NoteInput.js
+++ src/components/admin/NoteInput.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import Button from "../button/Button";
 
 const NoteInput = (props) => {
     const [description, setDescription] = useState('');
+    const textAreaRef = useRef(null);
+
+    const onAdd = () => {
+        props.onAdd(description);
+        textAreaRef.current.value = '';
+        textAreaRef.current.focus();
+    }
 
     return (
         <div>
-            <textarea cols="60" onChange={e => setDescription(e.target.value)} name="note"/>
-            <Button type="button" onClick={() => props.onAdd(description)}>Add</Button>
+            <textarea ref={textAreaRef} cols="60" onChange={e => setDescription(e.target.value)} name="note"/>
+            <Button type="button" onClick={onAdd}>Add</Button>
         </div>
     );
 };
