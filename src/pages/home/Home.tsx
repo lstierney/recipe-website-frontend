bfd32e8f44341ba47f4f627841d5567b18ecc8b4
Lawrence Tierney <lawrence.tierney@protonmail.com>
2023-09-06 23:17:59 +0100

    MVR-278 Support "Cooked"

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,56 +1,15 @@
-import {useGetLatestRecipesQuery, useGetRecipeTitlesAndIdsQuery} from "../../store/api";
+import {useGetLatestRecipesQuery} from "../../store/api";
 import RecipesList from "../../components/recipe/recipeslist/RecipesList";
 import mainClasses from '../../main.module.css';
-import {useEffect, useState} from "react";
-import _ from 'lodash';
-import {useNavigate} from "react-router-dom";
-import {RecipeType} from "../../types/recipeType";
+import Hero from "../../components/hero/Hero";
 
 
 const HomePage = () => {
     const {data: latestRecipes} = useGetLatestRecipesQuery({});
-    const {data: images, isSuccess} = useGetRecipeTitlesAndIdsQuery({});
-    const [randomRecipe, setRandomRecipe] = useState<RecipeType | null>(null);
-    const imgHost = process.env.REACT_APP_API_HOST;
-    const navigate = useNavigate()
-
-    const handleImageClick = (name: string) => {
-        name = name.replace(/ /g, '-').toLowerCase();
-        navigate(`/recipes/${name}`);
-    };
-
-    useEffect(() => {
-        if (isSuccess) {
-            setRandomRecipe(_.sample(images))
-        }
-        const timer = setInterval(() => {
-            setRandomRecipe(_.sample(images));
-        }, 10000); // Change image every 10 seconds
-
-        return () => clearInterval(timer); // Clean up timer on unmount
-    }, [images, isSuccess]);
 
     return (
         <>
-            {randomRecipe &&
-                <div onClick={() => handleImageClick(randomRecipe.name)}
-                     className={mainClasses['hero-image-container']}>
-
-                    <img className={mainClasses['hero-image']} src={imgHost + '/images/' + randomRecipe.imageFileName}
-                         alt={randomRecipe.name}/>
-
-                </div>
-            }
-            {randomRecipe &&
-                <div onClick={() => handleImageClick(randomRecipe.name)} className={mainClasses['hero-card']}>
-
-                    <div className={mainClasses['hero-card-inner']}>
-                        <div className={mainClasses['hero-card-name']}>{randomRecipe.name}</div>
-                        <div className={mainClasses['hero-card-description']}>{randomRecipe.description}</div>
-                    </div>
-
-                </div>
-            }
+            <Hero/>
             <div className={mainClasses['home-previews']}>
                 <RecipesList recipes={latestRecipes ? latestRecipes : []}/>
             </div>
